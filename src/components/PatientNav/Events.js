8d14eee0cb13edde756fbb542a8ea612b31c8a89
joshthepirate <jenninjl01@gmail.com>
2019-05-08 16:34:26 +0100

    Update Events.js

--- src/components/PatientNav/Events.js
+++ src/components/PatientNav/Events.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import {
+  Alert,
+  FlatList,
   LayoutAnimation,
   StatusBar,
   StyleSheet,
@@ -16,6 +18,8 @@ import {
   SafeAreaView,
 } from 'react-navigation';
 import { Button } from '../commonComponents/ButtonWithMargin';
+//import Modal from "react-native-modal";
+import DateTimePicker from "react-native-modal-datetime-picker";
 
 interface Props {
   navigation: NavigationScreenProp<NavigationState>;
@@ -39,15 +43,119 @@ export class EventsScreen extends React.Component<Props> {
         style={{ color: tintColor }}
       />
     ),
+    
   };
-  render() {	  
-    const { navigation } = this.props;
+  state = {
+    isModalVisible: false,
+    eventsArray: []
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDateTimePickerVisible: false,
+      eventsArray: []
+    };
+  }
+
+  showDateTimePicker = () => {
+    this.setState({ isDateTimePickerVisible: true });
+  };
+
+  hideDateTimePicker = () => {
+    this.setState({ isDateTimePickerVisible: false });
+  };
+
+  handleDatePicked = dateTime => {
+    console.log("A date has been picked: ", dateTime);
+    var unixDateAndTime = Math.floor(dateTime.getTime() / 1000);
+    var currentTime = Math.round((new Date()).getTime() / 1000);
+    if (unixDateAndTime < currentTime) {
+      Alert.alert(
+        'Invalid time and date',
+        'You cannot make an event in the past.',
+        [
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: false }
+      ); 
+      this.hideDateTimePicker();
+    } else {
+      var newDateAndTime = unixDateAndTime.toString();
+      var events = this.state.eventsArray;
+      var joined = [{key : newDateAndTime}].concat(events);
+      this.setState({ eventsArray : joined.sort()}); // In Unix Timestamp Mode so that sort works correctly
+      this.hideDateTimePicker();
+    }
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
+  deleteItem = data => {
+    let allItems = this.state.eventsArray;
+    let filteredItems = allItems.filter(item => item.key != data.key);
+    this.setState({ eventsArray: filteredItems })
+  }
+
+  deleteItemAlert = item => {
+    Alert.alert(
+      'Warning',
+      'Are you sure you would like to delete that event?',
+      [
+        {text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'Yes', onPress: () => this.deleteItem(item)},
+      ],
+      { cancelable: false }
+    )
+  }
+
+  render() {
     return (
-      <SafeAreaView forceInset={{ horizontal: 'always', top: 'always' }}>
-        <Text>Events Screen</Text>
-      </SafeAreaView>
+      <View style={{ 
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        }}>
+        <Text>Note, these times are in Unix time:</Text>
+
+        <View style={styles.container}>
+        <FlatList
+          data={this.state.eventsArray}
+          renderItem={({item}) => (
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={styles.item}>{item.key}</Text>
+              <Button title="Delete" onPress={() => this.deleteItemAlert(item)} /> 
+          </View> )}
+        />
+      </View>
+
+        <Button title="Add Event" onPress={this.showDateTimePicker} />
+            <DateTimePicker
+              isVisible={this.state.isDateTimePickerVisible}
+              onConfirm={this.handleDatePicked}
+              onCancel={this.hideDateTimePicker}
+              mode={'datetime'}
+            />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+// Need to save and initialise eventsArray from a server - Also need to account for multiple people using it at the same time?
+// Need to send a notification 1 hour before an event
 
 export default EventsScreen
